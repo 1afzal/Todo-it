0f78102eaa0cd419b901b54a582ad25ba089ab88
1afzal <afzalali5256@gmail.com>
2025-11-27 14:20:28 +0530

    fixed todo update

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -2,22 +2,35 @@ import { Router } from "express";
 const todoRouter = Router();
 import { todoModel } from "../models/todoModel.js";
 
-todoRouter.get("/all", async(req, res) => {
-  try{
-    const todos = await todoModel.find({})
+todoRouter.get("/all", async (req, res) => {
+  try {
+    const todos = await todoModel.find({});
     res.status(200).json({
-      data: todos 
-    })
-  }
-  catch(err){
+      data: todos,
+    });
+  } catch (err) {
     console.log(err.message);
     res.status(400).json({
-      message: err.message
-    })
+      message: err.message,
+    });
   }
 });
 
-todoRouter.get("/todo/:id", () => {});
+todoRouter.get("/todo/:id", async (req, res) => {
+  try {
+    const idx = req.params.id;
+    const todo = await todoModel.findById(idx);
+    res.status(200).json({
+      message: `todo fetch successfull`,
+      data: todo,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(200).json({
+      message: err.message,
+    });
+  }
+});
 
 todoRouter.post("/post/todo", async (req, res) => {
   try {
@@ -29,20 +42,47 @@ todoRouter.post("/post/todo", async (req, res) => {
       duedate: req.body.duedate,
       user: req.body.user,
     });
-    console.log(`New todo created -> ${newTodo.title}`)
+    console.log(`New todo created -> ${newTodo.title}`);
     res.status(200).json({
-        message: "new todo created",
-        title: newTodo.title
-    })
+      message: "new todo created",
+      title: newTodo.title,
+    });
   } catch (err) {
     console.log(`Todo post error: ${err.message}`);
     res.status(400).send({
-        message: err.message
-    })
+      message: err.message,
+    });
   }
 });
 
-todoRouter.put("/update/todo", (req, res) => {});
+todoRouter.put("/update/todo/:id", async (req, res) => {
+  try {
+    const idx = req.params.id;
+    const updatedTodo = await todoModel.findByIdAndUpdate(
+      idx,{
+        title:req.body.title,
+        description:req.body.description,
+        isCompleted:req.body.isCompleted,
+        priority:req.body.priority,
+        duedate:req.body.duedate,
+        user:req.body.user
+      },
+      {new:true, runValidators: true}
+    );
+    if(!updatedTodo){
+      return res.status(404).json({
+        message: "Todo not found"
+      });
+    }
+    res.status(200).json({
+      message: "todo is updated successfully",
+    });
+  } catch (err) {
+    res.status(200).json({
+      message: err.message,
+    });
+  }
+});
 
 todoRouter.delete("/todo/delete", (req, res) => {});
 export default todoRouter;
